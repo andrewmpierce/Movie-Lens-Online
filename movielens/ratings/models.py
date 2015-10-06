986b1595736bbb308c90e3b788ca27138a03dcb4
apierce1992 <apierce1992@gmail.com>
2015-10-06 14:15:22 -0400

    added ratings

--- movielens/ratings/models.py
+++ movielens/ratings/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
+class Raters(models.Model):
+    pass
+
+class Movies(models.Model):
+    pass
+
+class Ratings(models.Model):
+    pass
+
+
+
 # Create your models here.
