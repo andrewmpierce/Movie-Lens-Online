bee45575e50c83dab3e82f2ba58a57e561dd9b24
apierce1992 <apierce1992@gmail.com>
2015-10-07 18:58:16 -0400

    omg data

--- movieratings/ratings/models.py
+++ movieratings/ratings/models.py
@@ -17,6 +17,8 @@ class Rater(models.Model):
     age = models.IntegerField()
     occupation = models.CharField(max_length=50)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default='X')
+    zipcode = models.CharField(max_length=5)
+
 
     def __str__(self):
         return str(self.id)
@@ -24,7 +26,7 @@ class Rater(models.Model):
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
-    
+
     def average_rating(self):
         return self.rating_set.aggregate(models.Avg('stars'))['stars__avg']
 
@@ -39,3 +41,81 @@ class Rating(models.Model):
 
     def __str__(self):
         return ' Title : {}, Rating: {}, Rater {}'.format(self.movie, self.stars, self.user)
+
+
+def load_ml_data():
+    import csv
+    import json
+    import re
+
+    users = []
+
+    with open('data/users.dat') as f:
+
+        reader = csv.DictReader([line.replace('::', '\t') for line in f],
+                                fieldnames='UserID::Gender::Age::Occupation::Zip-code'.split('::'),
+                                delimiter='\t')
+        for row in reader:
+            user = {
+                'fields': {
+                    'gender': row['Gender'],
+                    'age': row['Age'],
+                    'occupation': row['Occupation'],
+                    'zipcode': row['Zip-code'],
+                },
+                'model': 'ratings.Rater',
+                'pk': int(row['UserID']),
+            }
+
+            users.append(user)
+
+    with open('fixtures/users.json', 'w') as f:
+        f.write(json.dumps(users))
+
+    #print(json.dumps(users, sort_keys=True, indent=4, separators=(',', ': ')))
+
+
+    ratings = []
+
+    with open('data/ratings.dat') as f:
+        count = 1
+        reader = csv.DictReader([line.replace('::', '\t') for line in f],
+                                fieldnames='UserID::MovieID::Rating::Timestamp'.split('::'),
+                                delimiter='\t')
+        for row in reader:
+            rating = {
+                'fields': {
+                    'stars': row['Rating'],
+                    'user': row['UserID'],
+                    'movie': row['MovieID']
+                },
+                'model': 'ratings.Rating',
+                'pk': count
+            }
+
+            ratings.append(rating)
+            count += 1
+
+    with open('ratings.json', 'w') as f:
+        f.write(json.dumps(ratings))
+
+
+    movies = []
+
+    with open('data/movies.dat', encoding='windows-1252') as f:
+        reader = csv.DictReader([line.replace('::', '\t') for line in f],
+                                fieldnames='MovieID::Title::Genres'.split('::'),
+                                delimiter='\t')
+        for row in reader:
+            movie = {
+                'fields': {
+                    'title': row['Title']
+                },
+                'model': 'ratings.Movie',
+                'pk': row['MovieID']
+            }
+
+            movies.append(movie)
+
+    with open('movies.json', 'w') as f:
+        f.write(json.dumps(movies))
