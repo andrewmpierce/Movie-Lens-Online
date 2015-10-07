ebc55a70bd2fa8b48fd8b03d4c236cd71e727569
apierce1992 <apierce1992@gmail.com>
2015-10-06 23:51:12 -0400

    added some methods

--- movieratings/ratings/models.py
+++ movieratings/ratings/models.py
@@ -2,28 +2,24 @@ from django.db import models
 
 
 class Raters(models.Model):
-    pass
+    age = models.IntegerField()
+    occupation = models.CharField(max_length=50)
 
-
-
-
-
+    def __str__(self):
+        return ' Age : {}, Occupation:{}'.format(self.age, self.occupation)
 
 
 class Movies(models.Model):
-    text = models.CharField(max_length=100)
-
-
-
-
-
-
-
+    title = models.CharField(max_length=100)
+    stars = models.ForeignKey(Ratings)
+    
+    def __str__(self):
+        return ' Title : {}'.format(self.title)
 
 
 class Ratings(models.Model):
-    pass
+    movie = models.ForeignKey(Movies)
+    rating = models.IntegerField()
 
-
-
-# Create your models here.
+    def __str__(self):
+        return ' Title : {}, Rating: {}'.format(self.movie, self.rating)
