ed4d86503a0c87099a7b603f7cfaccb217fc3079
apierce1992 <apierce1992@gmail.com>
2015-10-07 10:35:37 -0400

    fixed add movie bug

--- movieratings/ratings/models.py
+++ movieratings/ratings/models.py
@@ -19,23 +19,23 @@ class Rater(models.Model):
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default='X')
 
     def __str__(self):
-        return ' Age : {}, Occupation:{}'.format(self.age, self.occupation)
+        return str(self.id)
 
 
 class Movie(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=255)
 
     def average_rating(self):
-        return self.rating_set.aggregate(models.Avg('Stars'))['stars__avg']
+        return self.rating_set.aggregate(models.Avg('stars'))['stars__avg']
 
     def __str__(self):
         return ' Title : {}'.format(self.title)
 
 
 class Rating(models.Model):
-    movie = models.ForeignKey('Movie')
-    user = models.ForeignKey('Rater')
-    stars = models.IntegerField(default=0)
+    movie = models.ForeignKey(Movie)
+    user = models.ForeignKey(Rater)
+    stars = models.IntegerField()
 
     def __str__(self):
         return ' Title : {}, Rating: {}, Rater {}'.format(self.movie, self.stars, self.user)
