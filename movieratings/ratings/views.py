e3f73acbea7b2d9c3dc155715f5164dcef036b9b
apierce1992 <apierce1992@gmail.com>
2015-10-08 10:03:56 -0400

    movie page

--- movieratings/ratings/views.py
+++ movieratings/ratings/views.py
@@ -8,18 +8,24 @@ def top_20(request):
     top_twenty = [movie for movie in Movie.objects.all() if type(movie.average_rating()) == float]
     top_twenty = sorted(top_twenty, key = lambda c: c.average_rating(), reverse=True)
     top_twenty = [str(movie) for movie in top_twenty[:20]]
-    return HttpResponse('<br>'.join(top_twenty))
-
+    #return HttpResponse('<br>'.join(top_twenty))
+    return render(request,
+                  'top_20.html',
+                   {'top_twenty':top_twenty})
 
 def show_movies(request, movie_id):
     movie = Movie.objects.get(pk=movie_id)
     string = ' <center><h3> Title : {} </h3> Average Rating : {}</center>'.format(movie.title, movie.average_rating())
-    #ratings = Rating.objects.get(movie = movie_id)
-    return HttpResponse(string)
-
+    #return HttpResponse(string)
+    return render(request,
+                  'movies.html',
+                   {'movie':movie})
 
 
 def users(request, user_id):
     user = Rater.objects.get(pk=user_id)
     string = '<center> <h3> User Id : {} </h3> Age : {} <br> Occupation : {} <br> Gender : {} <br> Zipcode : {}</center>'.format(user.id, user.age, user.occupation, user.gender, user.zipcode)
-    return HttpResponse(string)
+    #return HttpResponse(string)
+    return render(request,
+                  'users.html',
+                   {'user':user})
