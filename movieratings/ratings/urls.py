56967a6d64b5822cae9ac6bfa484b788093a5696
apierce1992 <apierce1992@gmail.com>
2015-10-09 11:37:26 -0400

    users branch is okay

--- movieratings/ratings/urls.py
+++ movieratings/ratings/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     #url(r'^top_20.html$', views.top_twenty, name='top_twenty'),
-    url(r'^top_20', views.top_20, name='top_twenty'),
-    url(r'^movies/(?P<movie_id>\d+)$', views.movie_detail, name='movie_detail'),
-    url(r'^users/(?P<user_id>\d+)$', views.rater_detail, name='rater_detail')
-]
+    url(r'movie/(?P<movie_id>\d+)$', views.movie_detail, name='movie_detail'),
+    url(r'users/(?P<rater_id>\d+)$', views.rater_detail, name='rater_detail'),
+    url(r'$', views.top_20, name='top_20')
+    ]
