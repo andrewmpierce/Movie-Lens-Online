b1f16f42cb37629c18f40dc69f90f6b6abadce7b
apierce1992 <apierce1992@gmail.com>
2015-10-08 20:40:50 -0400

    adding users stuff

--- movieratings/movieratings/urls.py
+++ movieratings/movieratings/urls.py
@@ -20,6 +20,7 @@ from ratings import views
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^r/', include('ratings.urls')),
+    url(r'^u/', include('users.urls')),
     url(r'^top_20', views.top_20, name='top_twenty'),
     url(r'^movies/(?P<movie_id>\d+)$', views.movie_detail, name='movie_detail'),
     url(r'^users/(?P<user_id>\d+)$', views.rater_detail, name='rater_detail')
