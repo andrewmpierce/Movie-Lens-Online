add3e603ad0c6f9c9d72c01d79c7018057ed1753
apierce1992 <apierce1992@gmail.com>
2015-10-10 17:28:45 -0400

    set up users app

--- moviedb/database/urls.py
+++ moviedb/database/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^top', views.top_movies, name='top_twenty'),
+    url(r'', views.top_movies, name='top_twenty'),
     url(r'^movies/(?P<movie_id>\d+)$', views.movie_detail, name='movie_detail'),
     url(r'^users/(?P<rater_id>\d+)$', views.rater_detail, name='rater_detail')
 ]
