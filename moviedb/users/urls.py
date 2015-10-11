620a33f2aecb204ceddfbaeccaf0061e244345b1
apierce1992 <apierce1992@gmail.com>
2015-10-11 12:23:43 -0400

    added login and reg

--- moviedb/users/urls.py
+++ moviedb/users/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^login', views.login, name='login'),
-    url(r'^register', views.register, name='register')
+    url(r'^login', views.user_login, name='login'),
+    url(r'^register', views.user_register, name='register')
 ]
--- moviedb/users/views.py
+++ moviedb/users/views.py
@@ -1,8 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import UserForm
 
 # Create your views here.
-def register(request):
-    pass
+def user_register(request):
+    form = UserForm(request.POST)
+    if form.is_valid():
+        user = form.save()
+        password = user.password
+        user.set_password(password)
+        user.save()
 
-def login(request):
-    pass
+        profile = Profile(user=user, fav_movie= 'Toy Story')
+        profile.save()
+
+        user = authenticate(username=user.username,
+                            password=password)
+
+        login(request, user)
+        return redirect('profile')
+    else:
+        form = UserForm()
+    return render(request, 'users/register.html',
+                  {'form':form})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user.is_active:
+            login(request, user)
+            return redirect('profile')
+        else:
+            return render(request,
+                      'users/login.html',
+                      {'failed':True,
+                      'username':username})
+    return render(request, 'users/login.html')
